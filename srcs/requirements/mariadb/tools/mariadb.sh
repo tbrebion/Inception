4370b79aabac9b412ceab56132cd80d374ad8df0
Titouan BREBION <tbrebion@e1r4p8.clusters.42paris.fr>
2023-01-26 19:02:21 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,35 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-mysql_install_db
+service mysql start;
 
-/etc/init.d/mysql start
+mysql -e "CREATE DATABASE IF NOT EXISTS \ `${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \ `${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
 
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-then
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-	echo "database already exists"
-
-else
-
-mysql_secure_installation << _EOF_
-
-Y
-root123
-root123
-Y
-n
-Y
-Y
-_EOF_
-
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-
-fi
-
-/etc/init.d/mysql stop
-
-exec "$@"
+exec mysqld_safe
