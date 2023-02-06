619212279fcbfe58b58b24955625e4c4a2ec019c
Titouan BREBION <tbrebion@e2r8p16.clusters.42paris.fr>
2023-02-06 18:07:55 +0100

    wip

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('SQL_NAME') );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', getenv('SQL_USER') );
+define( 'DB_USER', getenv('DB_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('SQL_PASSWORD') );
+define( 'DB_PASSWORD', getenv('DB_USER_PASSWD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('SQL_HOSTNAME'));
+define( 'DB_HOST', getenv('MARIADB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
