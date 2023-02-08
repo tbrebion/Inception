2c1b5bb1f0ffe6d40717f582126efe9e84b1aa6c
Titouan BREBION <tbrebion@e1r4p23.clusters.42paris.fr>
2023-02-08 17:46:59 +0100

    wip

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-# if [ -f "/var/www/wordpress/.is_wp_installed" ]
+if [ -f "/var/www/wordpress/.is_wp_installed" ]
 
-# then
-# 	echo "Wordpress is already installed"
+then
+	echo "Wordpress is already installed"
 
-# else
-sleep 10
-echo "STEP CONFIG CREATE"
-wp config create --allow-root \
-			--dbname=$SQL_DATABASE \
-			--dbuser=$SQL_USER \
-			--dbpass=$SQL_PASSWORD \
-			--dbhost=mariadb:3306 --path=/var/www/wordpress
+else
+	sleep 10
+	echo "STEP CONFIG CREATE"
+	wp config create --allow-root \
+				--dbname=$SQL_DATABASE \
+				--dbuser=$SQL_USER \
+				--dbpass=$SQL_PASSWORD \
+				--dbhost=mariadb:3306 --path=/var/www/wordpress
 
-echo "STEP CORE INSTALL"
-wp core install --path=/var/www/wordpress \
-			--url=https://${DOMAIN_NAME} \
-			--title=Inception \
-			--admin_user=${WP_ADMIN_USER} \
-			--admin_password=${WP_ADMIN_PASS} \
-			--admin_email=${WP_ADMIN_EMAIL} \
-			--skip-email \
-			--allow-root
+	echo "STEP CORE INSTALL"
+	wp core install --path=/var/www/wordpress \
+				--url=https://${DOMAIN_NAME} \
+				--title=Inception \
+				--admin_user=${WP_ADMIN_USER} \
+				--admin_password=${WP_ADMIN_PASS} \
+				--admin_email=${WP_ADMIN_EMAIL} \
+				--skip-email \
+				--allow-root
 
-echo "Adding another user"
-echo "STEP USER CREATE"
-wp user create ${WP_USER} ${WP_EMAIL} \
-			--role=author \
-			--path=/var/www/wordpress \
-			--user_pass=${WP_PASS} \
-			--display_name=${WP_USER} \
-			--first_name=${WP_USER_FIRST_NAME} \
-			--last_name=${WP_USER_LAST_NAME} \
-			--allow-root
+	echo "Adding another user"
+	echo "STEP USER CREATE"
+	wp user create ${WP_USER} ${WP_EMAIL} \
+				--role=author \
+				--path=/var/www/wordpress \
+				--user_pass=${WP_PASS} \
+				--display_name=${WP_USER} \
+				--first_name=${WP_USER_FIRST_NAME} \
+				--last_name=${WP_USER_LAST_NAME} \
+				--allow-root
 
-touch /var/www/wordpress/.is_wp_installed
+	touch /var/www/wordpress/.is_wp_installed
 
-# fi
+fi
 
 # php-fpm7.3 -F
 exec /usr/sbin/php-fpm7.3 -F
