446bf2c3b4b84e7548b14c875ca92cd547fd9c59
tbrebion <titouan.brebion@yahoo.com>
2023-02-09 18:35:42 +0100

    wip

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -7,12 +7,12 @@
 
 # else
 	sleep 10
-	# echo "STEP CONFIG CREATE"
-	# wp config create --allow-root \
-	# 			--dbname=$SQL_DATABASE \
-	# 			--dbuser=$SQL_USER \
-	# 			--dbpass=$SQL_PASSWORD \
-	# 			--dbhost=mariadb:3306 --path=/var/www/wordpress
+	echo "STEP CONFIG CREATE"
+	wp config create --allow-root \
+				--dbname=$SQL_DATABASE \
+				--dbuser=$SQL_USER \
+				--dbpass=$SQL_PASSWORD \
+				--dbhost=mariadb:3306 --path=/var/www/wordpress
 
 	echo "STEP CORE INSTALL"
 	wp core install --path=/var/www/wordpress \
