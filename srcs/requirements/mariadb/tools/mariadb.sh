2dfd0d71425f562d0f3530530c99c36f930ba0f9
tbrebion <titouan.brebion@yahoo.com>
2023-02-10 18:50:52 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -7,7 +7,7 @@ mysql_install_db
 	mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 	mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-	mysql -u "$SQL_USER" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+	mysql -u "wordpress.srcs_inception" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 	mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
 # mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
 # mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
