84962a964d18d131bd66e381e24063ca71dc2cc4
tbrebion <titouan.brebion@yahoo.com>
2023-02-10 18:43:56 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -9,6 +9,7 @@ mysql_install_db
 	# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 	# mysql -u "$MYSQL" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 	# mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
+mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
