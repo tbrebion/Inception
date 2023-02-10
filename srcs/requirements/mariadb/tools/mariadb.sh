b599eb0e144199ff330fa24ff071b1000e3c7f76
tbrebion <titouan.brebion@yahoo.com>
2023-02-10 14:58:56 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -9,4 +9,4 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' I
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -uroot -p$"SQL_ROOT_PASSWORD" shutdown
