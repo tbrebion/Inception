72f4d139e999ac5a5c6fb7a81854e8efcee00015
tbrebion <titouan.brebion@yahoo.com>
2023-02-13 19:49:52 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 mysql_install_db
+
+/etc/init.d/mysql start
+
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
-sleep 5
+
 /usr/bin/mysql_secure_installation
 
-mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
+# mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -uroot -p$"SQL_ROOT_PASSWORD" shutdown
+
