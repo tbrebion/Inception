2310a02cd2cfb5a00fc01eb5a2ec973e02e36a82
tbrebion <titouan.brebion@yahoo.com>
2023-02-13 20:09:24 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mysql_install_db
-/usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+/usr/bin/mysql_secure_installation
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
@@ -10,7 +10,7 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -u "$SQL_USER" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
 
-/usr/bin/mysql_secure_installation
+# /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
 
 # mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
 # mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
