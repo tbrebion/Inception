b97729692f236ca599d1d9b994a4e27edcba8b86
tbrebion <titouan.brebion@yahoo.com>
2023-02-14 18:48:19 +0100

    just after it works

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,45 +1,20 @@
 #!/bin/bash
 
-# mysql_install_db
+if [ -f "/var/lib/mysql/.already_init" ]; then
+	echo "Database is already initialized."
+else	
+	service mysql start;
 
+	sleep 5
 
-# /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
-# # /usr/bin/mysqld_safe
-# /usr/bin/mysql_secure_installation
+	mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+	mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+	mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+	mysql -u "$MYSQL" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+	mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
+	touch /var/lib/mysql/.already_init
 
-service mysql start;
-
-
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -u "$MYSQL" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
-
+fi
 
 exec mysqld
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_ADMIN_USER}\`@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -u "$SQL_USER" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-# mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
-
-
-
-# while ! mysqladmin ping -hlocalhost --silent; do
-#     sleep 1
-# done
-# sleep 15;
-
-
-
-# mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-# mysqladmin -uroot -p$"SQL_ROOT_PASSWORD" shutdown
