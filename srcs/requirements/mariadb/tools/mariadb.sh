644aa8038e33b58e64f9b540739287e19febb934
tbrebion <titouan.brebion@yahoo.com>
2023-02-14 15:57:04 +0100

    wip

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,36 +4,21 @@ mysql_install_db
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
 /usr/bin/mysql_secure_installation
 
-sleep 5
 
-mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -uroot -p$"SQL_ROOT_PASSWORD" shutdown
+mysql -u "$SQL_USER" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
 
 
-#!/bin/bash
-
-# mysql_install_db
-
-# /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
-
-# /usr/bin/mysql_secure_installation
 
 # while ! mysqladmin ping -hlocalhost --silent; do
     # sleep 1
 # done
 # sleep 15;
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -u "$SQL_USER" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-# mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
 
 
 # mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e
@@ -43,10 +28,3 @@ mysqladmin -uroot -p$"SQL_ROOT_PASSWORD" shutdown
 # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # mysql -e "FLUSH PRIVILEGES;"
 # mysqladmin -uroot -p$"SQL_ROOT_PASSWORD" shutdown
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -u "$SQL_USER" -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-# mysqladmin -uroot -p"$SQL_ROOT_PASSWORD" shutdown
